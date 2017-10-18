de4d675ec1e2083859f14d4a4bd49b9cbc679c82
ChiChaChai <chatchaishaetan@gmail.com>
2017-10-19 04:35:45 +0700

    add check circle collision

--- TestGame/main.cpp
+++ TestGame/main.cpp
@@ -1,29 +1,32 @@
-#include<SFML\Graphics.hpp>
-#include<SFML\System.hpp>
-#include<iostream>
-#include<conio.h>
+#include <SFML\Graphics.hpp>
+#include <SFML\System.hpp>
+#include <iostream>
+#include <conio.h>
 #include "Physics.h"
+#include <math.h>
 using namespace sf;
 using namespace std;
 using namespace abc;
 
-
+bool checkCircleCollision(int x1, int y1, float radius1, int x2, int y2, float radius2)
+{
+	return radius1 + radius2 > sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+}
 int main()
 {
 	Clock Clk;
 	Time t = Clk.getElapsedTime();
-	Int32 tint=0;
-	Int32 temp=0;
+	Int32 tint = 0;
+	Int32 temp = 0;
 	Physics p;
-	
 
 	ContextSettings csx;
 	csx.antialiasingLevel = 10;
-	RenderWindow window(VideoMode(1200,800), "TestGame");
+	RenderWindow window(VideoMode(1200, 800), "TestGame");
 	CircleShape a(50);
 	a.setFillColor(Color(60, 255, 185));
-	float i=0, j=0,speed=1;
-	Vector2f test=Vector2f(i,j);
+	float i = 0, j = 0, speed = 1;
+	Vector2f test = Vector2f(i, j);
 	a.setPosition(test);
 
 	while (window.isOpen())
@@ -42,7 +45,6 @@ int main()
 			else if (Keyboard::isKeyPressed(Keyboard::A))
 			{
 				i -= speed;
-				
 			}
 			else if (Keyboard::isKeyPressed(Keyboard::S))
 			{
@@ -63,36 +65,35 @@ int main()
 			else if (Keyboard::isKeyPressed(Keyboard::L))
 			{
 				speed -= 5;
-				if (speed <=0)
+				if (speed <= 0)
 				{
 					speed = 1;
 				}
 			}
 			else if (Keyboard::isKeyPressed(Keyboard::Space))
 			{
-				Thread thr(&projectile,&a);
+				Thread thr(&projectile, &a);
 				thr.launch();
-/*				for (int l = 1; l < 1000;l++)
+				/*				for (int l = 1; l < 1000;l++)
 				{
 					cout << l;
 				}*/
-//				p.Projectile(&a);
-//				projectile(&a);
-/*				j = 800-2*a.getRadius();
+				//				p.Projectile(&a);
+				//				projectile(&a);
+				/*				j = 800-2*a.getRadius();
 				t = Clk.getElapsedTime();
 				tint = t.asMilliseconds();
 				temp = tint + 500;*/
 				cout << "\na\n";
 			}
-			
+
 			Vector2f pcir = a.getPosition();
 			int d = tint - temp;
 			float cx = pcir.x, cy = pcir.y;
-			cout << "\n\nTime: " << tint<<","<<d<< "\nPos: " << cx<<","<<cy;
-
+			cout << "\n\nTime: " << tint << "," << d << "\nPos: " << cx << "," << cy;
 		}
-		
-/*		t = Clk.getElapsedTime();
+
+		/*		t = Clk.getElapsedTime();
 		tint = t.asMilliseconds();
 		int d = (temp - tint);
 		if (abs(d) <= 500)
@@ -106,11 +107,9 @@ int main()
 		window.clear();
 		test = Vector2f(i, j);
 		cout << "test\n";
-//		a.setPosition(i, j);
-//		p.setPos(&a, i, j);
+		//		a.setPosition(i, j);
+		//		p.setPos(&a, i, j);
 		window.draw(a);
 		window.display();
-		
-
 	}
 }
